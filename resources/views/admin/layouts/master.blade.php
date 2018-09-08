9715f02413bc3d98fca35f0bc5427f2379edc0c2
stevenlee97 <lytruonguy97@gmail.com>
2018-09-08 22:27:29 +0700

    hoang

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -300,12 +300,6 @@
                             </ul>
                         </li>
 
-                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-table"></i><span class="hide-menu">Tables</span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                <li><a href="table-bootstrap.html">Basic Tables</a></li>
-                                <li><a href="table-datatable.html">Data Tables</a></li>
-                            </ul>
-                        </li>
 
                     </ul>
                 </nav>
