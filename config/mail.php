92bb809a09a79eaf098b0944f731747e794d1c68
nguyenanhhoang97 <nguyenanhhoang.ict@gmail.com>
2018-09-10 22:38:00 +0700

    mailing

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'lytruonguy97ic@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Ly Truong Uy'),
     ],
 
     /*
